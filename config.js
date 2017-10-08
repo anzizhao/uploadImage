38016685e10bcb4f85484914100ecc34fb15dda9
zhaojunan <anzizhao@126.com>
2017-10-08 19:05:00 +0800

    feat: 第一版上传的功能完成

---
+++ config.js
@@ -0,0 +1,16 @@
+// upload config 
+exports.imageDir = '../../Picture'; 
+
+exports.categoryData = {
+    name: 'clientUploadImage',
+    inShort: 'just a test',
+    describe: 'describe, just a test',
+    coverImg: 'test.jpg',
+    symbolImgs: ['test.jpg','test2.jpg', 'test3.jpg'] 
+};
+
+// 客户端的配置
+exports.serverConfig = {
+    host: 'http://localhost:3001',
+    userId: '5750cbc575a3ef200a96cb16'
+};
