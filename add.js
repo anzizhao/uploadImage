6589d690abb565ae5f375201d5fc5abcb4249429
zhaojunan <anzizhao@126.com>
2017-10-13 08:02:22 +0800

    feat： 增加mod的逻辑

---
+++ add.js
@@ -0,0 +1,133 @@
+const config = require('./config');
+const { 
+    categoryData, 
+    serverConfig, 
+    imageDir
+} = config;
+const io = require('socket.io-client');
+const ss = require('socket.io-stream');
+const fs = require('fs');
+const uuid = require('uuid');
+const path = require('path');
+const sizeOf = require('image-size');
+const glob = require('glob');
+
+export function add() {
+    let socket = '';
+    // 目录的id
+    let categoryId = '';
+    // 准备发送的文件队列
+    let fileQueue = [];
+    let sendNum = 0;
+    let coverImg = null;
+    let symbolImgs = null;
+
+    const startTime = Date.now();
+    // options is optional
+    glob(`${imageDir}/*`, null, function (err, files) {
+        if (err) {
+            console.log(imageDir, ' directory read error, ', err);
+            return;
+        }
+        fileQueue = files.map(item => {
+            // 生成对应的uuid
+            return {
+                filePath: item,
+                uuid: uuid.v1()
+            }
+        });
+        // 如果有设置目录和首三张，替换对应uuid
+        if (categoryData.coverImg) {
+            const result = fileQueue.find(item => path.basename(item.filePath) === categoryData.coverImg)
+            if (result) {
+                coverImg = {
+                    uuid: result.uuid    
+                } 
+            }
+        }
+        if (categoryData.symbolImgs) {
+            symbolImgs = categoryData.symbolImgs.map(sItem => {
+                const result = fileQueue.find(item => path.basename(item.filePath) === sItem )
+                const id = !!result ? result.uuid : '';
+                return {
+                    uuid: id
+                }
+            }).filter(item => !!item.uuid ); 
+        }
+        console.log(coverImg)
+        console.log(symbolImgs)
+        console.log('files number: ', fileQueue.length)
+        socket = io.connect(serverConfig.host);
+
+        socket.emit('pictureAdd_start');
+
+        let data = {
+            userId: serverConfig.userId,
+            name: categoryData.name || 'client upload image',
+            inShort: categoryData.inShort || 'just a test',
+            describe: categoryData.describe || 'describe, just a test',
+            coverImg,
+            symbolImgs
+        };
+
+        socket.emit('addCategory', data);
+        socket.on('addCategory_ret', (ret) => {
+            console.log('addCategory_ret: ', ret);
+            categoryId = ret.id;
+            const fileToSend = getFile();
+            if(fileToSend) {
+                sendImage(fileToSend);
+            }
+        })
+        socket.on('addCategoryImages_ret', (data) => {
+            if (data.error) {
+                console.log('addCategoryImages return error ', data)
+                return;
+            } 
+            console.log('addCategoryImages return ', sendNum, data)
+            const fileToSend = getFile();
+            if(fileToSend) {
+                sendImage(fileToSend);
+            } else {
+                socket.emit('pictureAdd_end');
+                console.log('pictureAdd_end');
+                const endTime = Date.now();
+                console.log(`upload images take times: ${(endTime-startTime)/ 1000}s`);
+                console.log(Date.now());
+                // 关闭链接
+                socket.close();
+            }
+        })
+
+        socket.on('disconnect', () => {
+            console.log('disconnect, ', Date.now());
+        });
+    })
+
+    function sendImage(opt) {
+        const filePath = opt.filePath;
+        if (!filePath) {
+            console.log('error, no file in the queue');
+            return;
+        }
+        // 读取图片准备上传
+        const stream = ss.createStream();
+        const info = sizeOf(filePath);
+        data = {
+            userId: serverConfig.userId,
+            categoryId,
+            uuid: opt.uuid,
+            width: info.width,
+            height: info.height 
+        };
+        ss(socket).emit('addCategoryImages', stream, data);
+        fs.createReadStream(filePath).pipe(stream);
+        console.log('addCategoryImages emit, ', sendNum, data.uuid);
+    }
+    function getFile() {
+        if (sendNum === fileQueue.length) {
+            return;
+        }
+        return fileQueue[sendNum++];
+    }
+};
--- config.js
+++ config.js
@@ -1,19 +1,28 @@
 // upload config 
-exports.imageDir = '/media/anzi/虚拟机/图片/金子'; 
-
-exports.categoryData = {
-    name: '连州市金子山游玩',
-    inShort: '遇到一群好玩的人',
-    describe: '有点像武功山',
-    coverImg: 'IMG_2840.JPG',
-    symbolImgs: ['IMG_2695.JPG', 'IMG_2761.JPG', 'IMG_2772.JPG'] 
-};
-
+exports.mode = 'mod'; // 'add' or 'mod'
+exports.imageDir = '/media/anzi/虚拟机/图片/惠州'; 
 // 客户端的配置
 exports.serverConfig = {
     host: 'http://www.anzizhao.com',
     userId: '5718487a86d9aa500fc696e5'
 };
+
+// use just in add mode
+exports.categoryData = {
+    name: '部门组织惠东游玩',
+    inShort: '一次难得的经历',
+    describe: '不知不觉，跟自媒体经历了这么多事情',
+    coverImg: 'IMG_2576.JPG',
+    symbolImgs: [
+        'IMG_2536.JPG', 
+        'IMG_2559.JPG', 
+        'IMG_2592.JPG'
+    ] 
+};
+// use just in mod mode
+exports.categoryId = '';
+
+
 // for test
 // exports.serverConfig = {
 //     host: 'http://localhost:3001',
