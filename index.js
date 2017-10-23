b5319982d76f0c316d5032dbdb1cddd1dab55aaa
赵俊安 <wb-zja257024@alibaba-inc.com>
2017-10-23 21:04:32 +0800

    feat: develop log

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const config = require('./config');
 const { add } = require('./add');
-const { mod } = require('./mod');
+const { mod, resume } = require('./mod');
+// 如果有日志文件，说明上一次没有上传完成，接着上传
+const fs = require('fs');
+const infoFile = config.serverConfig.infoFile;
+// info.json 文件是否存在， 存在说明上次上传没有完成
+if (fs.existsSync(infoFile)) {
+  // 文件存在，走续传逻辑
+  const resumeConf = require(infoFile);
+  return resume(resumeConf);
+} 
+
 if (config.mode === 'mod') {
     mod();
 } else {
--- mod.js
+++ mod.js
@@ -1,7 +1,8 @@
 const config = require('./config');
 const { 
     serverConfig, 
-    imageDir
+    imageDir,
+    infoFile
 } = config;
 const io = require('socket.io-client');
 const ss = require('socket.io-stream');
@@ -11,88 +12,110 @@ const path = require('path');
 const sizeOf = require('image-size');
 const glob = require('glob');
 
-exports.mod = () => {
-    let socket = '';
-    // 目录的id
-    let categoryId = config.categoryId;
-    // 准备发送的文件队列
-    let fileQueue = [];
-    let sendNum = 0;
-    let coverImg = null;
-    let symbolImgs = null;
 
-    const startTime = Date.now();
+let socket = '';
+// 目录的id
+let categoryId = config.categoryId;
+// 用户的id
+let userId = serverConfig.userId;
+// 准备发送的文件队列
+let fileQueue = [];
+let sendNum = 0;
+let coverImg = null;
+let symbolImgs = null;
+
+const startTime = Date.now();
+
+exports.mod = () => {
     // options is optional
-    glob(`${imageDir}/*`, null, function (err, files) {
+    glob(`${imageDir}/*`, null, (err, files) => {
         if (err) {
             console.log(imageDir, ' directory read error, ', err);
             return;
         }
-        fileQueue = files.map(item => {
-            // 生成对应的uuid
-            return {
-                filePath: item,
-                uuid: uuid.v1()
-            }
-        });
-        console.log('files number: ', fileQueue.length)
-        socket = io.connect(serverConfig.host);
+        // 记录info.json 文件
+        const info = {
+            categoryId,
+            userId,
+            files: files
+        };
+        const ws = fs.createWriteStream(infoFile);
+        ws.write(JSON.stringify(info, null, 4));
+        ws.end();
+        handleFile(config.files);
+    })
+};
+exports.resume = (config) => {
+    categoryId = config.categoryId;
+    userId = config.userId;
+    handleFile(config.files);
+};
+// options is optional
+function handleFiles(files) {
+    fileQueue = files.map(item => {
+        // 生成对应的uuid
+        return {
+            filePath: item,
+            uuid: uuid.v1()
+        }
+    });
+    console.log('files number: ', fileQueue.length)
+    socket = io.connect(serverConfig.host);
 
-        const fileToSend = getFile();
-        if(!fileToSend) {
-            // 如果没有图片需要发送，返回退出
+    const fileToSend = getFile();
+    if(!fileToSend) {
+        // 如果没有图片需要发送，返回退出
+        return;
+    } 
+    socket.emit('pictureMod_start');
+    socket.on('addCategoryImages_ret', (data) => {
+        if (data.error) {
+            console.log('addCategoryImages return error ', data)
             return;
         } 
-        socket.emit('pictureMod_start');
-        socket.on('addCategoryImages_ret', (data) => {
-            if (data.error) {
-                console.log('addCategoryImages return error ', data)
-                return;
-            } 
-            console.log('addCategoryImages return ', sendNum, data)
-            const fileToSend = getFile();
-            if(fileToSend) {
-                sendImage(fileToSend);
-            } else {
-                socket.emit('pictureMod_end');
-                console.log('pictureMod_end');
-                const endTime = Date.now();
-                console.log(`upload images take times: ${(endTime-startTime)/ 1000}s`);
-                console.log(Date.now());
-                // 关闭链接
-                socket.close();
-            }
-        })
-        socket.on('disconnect', () => {
-            console.log('disconnect, ', Date.now());
-        });
-        sendImage(fileToSend);
+        console.log('addCategoryImages return ', sendNum, data)
+        const fileToSend = getFile();
+        if(fileToSend) {
+            sendImage(fileToSend);
+        } else {
+            socket.emit('pictureMod_end');
+            console.log('pictureMod_end');
+            const endTime = Date.now();
+            console.log(`upload images take times: ${(endTime-startTime)/ 1000}s`);
+            console.log(Date.now());
+            // 关闭链接
+            socket.close();
+        }
     })
+    socket.on('disconnect', () => {
+        console.log('disconnect, ', Date.now());
+    });
+    sendImage(fileToSend);
+}
 
-    function sendImage(opt) {
-        const filePath = opt.filePath;
-        if (!filePath) {
-            console.log('error, no file in the queue');
-            return;
-        }
-        // 读取图片准备上传
-        const stream = ss.createStream();
-        const info = sizeOf(filePath);
-        data = {
-            userId: serverConfig.userId,
-            categoryId,
-            uuid: opt.uuid,
-            width: info.width,
-            height: info.height 
-        };
-        ss(socket).emit('addCategoryImages', stream, data);
-        fs.createReadStream(filePath).pipe(stream);
-        console.log('addCategoryImages emit, ', sendNum, data.uuid);
+function sendImage(opt) {
+    const filePath = opt.filePath;
+    if (!filePath) {
+        console.log('error, no file in the queue');
+        return;
     }
-    function getFile() {
-        if (sendNum === fileQueue.length) {
-            return;
-        }
-        return fileQueue[sendNum++];
+    // 读取图片准备上传
+    const stream = ss.createStream();
+    const info = sizeOf(filePath);
+    data = {
+        userId,
+        categoryId,
+        uuid: opt.uuid,
+        width: info.width,
+        height: info.height 
+    };
+    ss(socket).emit('addCategoryImages', stream, data);
+    fs.createReadStream(filePath).pipe(stream);
+    console.log('addCategoryImages emit, ', sendNum, data.uuid);
+}
+function getFile() {
+    if (sendNum === fileQueue.length) {
+        return;
     }
-};
+    return fileQueue[sendNum++];
+}
